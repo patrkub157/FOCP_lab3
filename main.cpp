db993aadd0459f277a52758080119d68b251ad23
patkub2 <patrykkubala1999@gmail.com>
2020-10-22 15:06:57 +0200

    max distance between points v2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <cstdlib>
 #include <iostream>
 #include <array>
@@ -22,12 +20,11 @@ Point2D create_point()
     return tmp;
 }
 
-float distance(Point2D left, Point2D right)
+float area_of_triangle(Point2D a, Point2D b, Point2D c)
 {
-    float res_x = pow((right.x - left.x), 2);
-    float res_y = pow((right.y - left.y), 2);
+    float product = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
 
-    return sqrt(res_x + res_y);
+    return abs(product / 2);
 }
 
 int main()
@@ -40,26 +37,31 @@ int main()
         cout << "point[" << i << "] = (" << points[i].x << ", " << points[i].y << ")" << endl;
     }
 
-    float largest_distance = 0.0;
-    Point2D first, second;
+    float largest_area = 0.0;
+    Point2D first, second, third;
 
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            float d = distance(points[i], points[j]);
-            if (d >= largest_distance)
+            for (int k = 0; k < 10; k++)
             {
-                largest_distance = d;
-                first = points[i];
-                second = points[j];
+                float area = area_of_triangle(points[i], points[j], points[k]);
+                if (area > largest_area)
+                {
+                    largest_area = area;
+                    first = points[i];
+                    second = points[j];
+                    third = points[k];
+                }
             }
         }
     }
 
-    cout << "The largest distance is: " << largest_distance << endl;
+    cout << "The largest area is: " << largest_area << endl;
     cout << "First point: (" << first.x << ", " << first.y << ")" << endl;
     cout << "Second point: (" << second.x << ", " << second.y << ")" << endl;
+    cout << "Third point: (" << third.x << ", " << third.y << ")" << endl;
 
     return 0;
 }
