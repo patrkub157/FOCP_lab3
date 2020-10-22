7648bf78c0712267dabca27c583491eb4e246aaf
patkub2 <patrykkubala1999@gmail.com>
2020-10-22 13:12:39 +0200

    max distance between points

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+
+
 #include <cstdlib>
 #include <iostream>
+#include <array>
+#include <cmath>
 
 using namespace std;
 
@@ -18,24 +22,44 @@ Point2D create_point()
     return tmp;
 }
 
-void initialize_in_place(Point2D &point)
+float distance(Point2D left, Point2D right)
 {
-    point.x = rand() % 10;
-    point.y = rand() % 10;
+    float res_x = pow((right.x - left.x), 2);
+    float res_y = pow((right.y - left.y), 2);
+
+    return sqrt(res_x + res_y);
 }
 
 int main()
 {
-    Point2D mypoint = create_point();
+    array<Point2D, 10> points;
 
-    cout << "Value of X: " << mypoint.x << endl;
-    cout << "Value of Y: " << mypoint.y << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        points[i] = create_point();
+        cout << "point[" << i << "] = (" << points[i].x << ", " << points[i].y << ")" << endl;
+    }
 
-    Point2D empty_point;
-    initialize_in_place(empty_point);
+    float largest_distance = 0.0;
+    Point2D first, second;
 
-    cout << "Value of X: " << empty_point.x << endl;
-    cout << "Value of Y: " << empty_point.y << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            float d = distance(points[i], points[j]);
+            if (d >= largest_distance)
+            {
+                largest_distance = d;
+                first = points[i];
+                second = points[j];
+            }
+        }
+    }
+
+    cout << "The largest distance is: " << largest_distance << endl;
+    cout << "First point: (" << first.x << ", " << first.y << ")" << endl;
+    cout << "Second point: (" << second.x << ", " << second.y << ")" << endl;
 
     return 0;
 }
